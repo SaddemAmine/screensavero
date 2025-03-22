4fd077b24cdd1497b328c4b2331bcb4c95c3d09f
SaddemAmine <sana@odoo.com>
2025-03-22 16:58:22 +0100

    init

---
+++ screensavero.lua
@@ -0,0 +1,15 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Screensavero
+--- MOD_ID: screensavero
+--- MOD_AUTHOR: [SaddemAmine, Nurpus]
+--- MOD_DESCRIPTION: Make the shader your screensaver.
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function SMODS.INIT.screensavero()
+    G.debug_UI_toggle = true
+end
+
+----------------------------------------------
+------------MOD CODE END----------------------
